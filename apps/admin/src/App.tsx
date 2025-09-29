7344e168964afa81b3fa6aef4d2528261e668dfc
imddoy <imddoy@duksung.ac.kr>
2025-09-29 20:19:32 +0900

    init: 디자인 토큰 컬러 세팅

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,14 +1,17 @@
-import './App.css'
+import { vars } from "@repo/ui/foundations/index.ts";
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p style={{ color: vars.colors.brand.orange[500] }}>
+        이 글씨는 오렌지색입니다.
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
