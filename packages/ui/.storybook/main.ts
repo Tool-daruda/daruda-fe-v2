e113b1c135be2b77cd2fcaa1c64bbeb9630f3fbd
goeun kim <rlarhdms6828@naver.com>
2025-10-20 03:18:25 +0900

    chore(ui): 스토리북 초기세팅시 작성된 docs 주석 삭제

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,10 +1,6 @@
 import { dirname, join } from "node:path";
 import type { StorybookConfig } from "@storybook/react-vite";
 
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
 function getAbsolutePath(value: string): any {
 	return dirname(require.resolve(join(value, "package.json")));
 }
