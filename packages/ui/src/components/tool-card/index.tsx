87db6aa2bae130288f5bc89faa4065330f696c9c
goeun kim <rlarhdms6828@naver.com>
2025-10-20 03:08:05 +0900

    feat(ui): tool card 컴포넌트 구현 및 storybook 랜더링

--- packages/ui/src/components/tool-card/index.tsx
+++ packages/ui/src/components/tool-card/index.tsx
@@ -1,9 +1,29 @@
 import * as styles from "./tool-card.css";
 
-const ToolCard = () => {
+interface ToolCardProps {
+	toolId: number;
+	toolLogo: string;
+	toolName: string;
+	description: string;
+	category: string;
+	updatedAt: string;
+}
+
+const ToolCard = ({ toolId, toolLogo, toolName, description, category, updatedAt }: ToolCardProps) => {
 	return (
 		<section className={styles.card}>
-			<h1>Tool Card Component</h1>
+			<div className={styles.cardHead}>
+				<span>{toolId}</span>
+				<img src={toolLogo} alt={toolName} className={styles.cardLogo}/>
+			</div>
+			<h1 className={styles.cardName}>{toolName}</h1>
+			<p className={styles.cardDescription}>{description}</p>
+			<span className={styles.cardCategory}>{category}</span>
+			<time className={styles.cardUpdatedAt}>{updatedAt}</time>
+			<div>
+				{/* TODO: 공통 컴포넌트 제작후 <Button>로 변경 */}
+				<button className={styles.editButton}>편집</button>
+			</div>
 		</section>
 	);
 };
--- packages/ui/src/components/tool-card/tool-card.css.ts
+++ packages/ui/src/components/tool-card/tool-card.css.ts
@@ -1,13 +1,79 @@
 import { style } from "@vanilla-extract/css";
-
 import { themeVars } from "../../foundations";
 
+const textClampSingleLine = {
+	display: "-webkit-box",
+	WebkitBoxOrient: "vertical" as const,
+	WebkitLineClamp: 2,
+	overflow: "hidden",
+	textOverflow: "ellipsis",
+	alignContent: "center",
+};
+
+const baseCardText = {
+	...themeVars.fonts.b5_1,
+};
+
 export const card = style({
-	color: themeVars.colors.brand.iris[700],
-	alignItems: "center",
-	gap: "0.8rem",
 	width: "100%",
-	position: "relative",
+	display: "flex",
+	padding: "10px 8px",
+	alignItems: "center",
+	gap: "44px",
+	alignSelf: "stretch",
+	borderBottom: `0.5px solid ${themeVars.colors.grayscale[50]}`,
+	color: themeVars.colors.grayscale[800],
+	background: themeVars.colors.grayscale[5],
+	...themeVars.fonts.b5_1,
+});
+
+export const cardHead = style({
+	display: "flex",
+	alignItems: "center",
+	gap: "16px",
+});
+
+export const cardLogo = style({
+	width: "60px",
+	height: "60px",
+	borderRadius: "8px",
+	objectFit: "cover",
+});
+
+export const cardName = style({
+	...textClampSingleLine,
+	width: "126px",
+	height: "40px",
+	...baseCardText,
+});
+
+export const cardDescription = style({
+	...textClampSingleLine,
+	width: "261px",
+	height: "40px",
+	...baseCardText,
+});
+
+export const cardCategory = style({
+	width: "150px",
+	...textClampSingleLine,
+});
+
+export const cardUpdatedAt = style({
+	width: "150px",
+	...textClampSingleLine,
+});
+
+export const editButton = style({
+	display: "flex",
+	padding: "8px 14px",
+	justifyContent: "center",
+	alignItems: "center",
+	borderRadius: "12px",
+	background: themeVars.colors.grayscale[50],
+	color: themeVars.colors.grayscale[700],
+	whiteSpace: "nowrap",
+	...themeVars.fonts.t4_1,
 	cursor: "pointer",
-	flexShrink: 0,
+	border: "none",
 });
