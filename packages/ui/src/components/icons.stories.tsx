e259c24ae49b8494093551b2258f3a351fe8f779
goeun kim <rlarhdms6828@naver.com>
2025-10-20 21:02:43 +0900

    feat(ui): svgr 초기 세팅 및 stories 구축

---
+++ packages/ui/src/components/icons.stories.tsx
@@ -0,0 +1,13 @@
+import ArrowLeft from "../assets/icons/arrow-left.svg?react";
+import ArrowRight from "../assets/icons/arrow-right.svg?react";
+
+export default {
+	title: "Foundations/Icons",
+};
+
+export const Icons = () => (
+	<div style={{ display: "flex", gap: "16px" }}>
+		<ArrowLeft />
+		<ArrowRight />
+	</div>
+);
