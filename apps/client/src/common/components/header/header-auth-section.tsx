b92897a521b5d09e1f5bb1d0b77d23bd06dc52d0
imddoy <imddoy@duksung.ac.kr>
2026-03-24 19:36:16 +0900

    refactor(client): 스크린리더 중복 읽기 방지

--- apps/client/src/common/components/header/header-auth-section.tsx
+++ apps/client/src/common/components/header/header-auth-section.tsx
@@ -20,10 +20,10 @@ export default function HeaderAuthSection({ isLoggedIn }: Props) {
 	return (
 		<div className={styles.authSection}>
 			<button type="button" aria-label="알림" className={styles.iconButton}>
-				<Image src="/icons/ic_alarm_gray500_28.svg" alt="Daruda 로고" width={28} height={28} />
+				<Image src="/icons/ic_alarm_gray500_28.svg" alt="" width={28} height={28} />
 			</button>
 			<button type="button" aria-label="마이페이지" className={styles.iconButton}>
-				<Image src="/icons/ic_profile_gray500_28.svg" alt="Daruda 로고" width={28} height={28} />
+				<Image src="/icons/ic_profile_gray500_28.svg" alt="" width={28} height={28} />
 			</button>
 		</div>
 	);
