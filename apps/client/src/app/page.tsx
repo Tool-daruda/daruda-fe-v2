6c94b0aac39032cf2f3089312acb6ec914d5f3e7
LeeHojin <dlghwls0203@naver.com>
2026-04-23 15:24:10 +0900

    BtPopup 버튼 구현

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { BtChipMainCategory } from "@/common/components/buttons/BtChipMainCategory";
 import { BtMyPageCommunityButton } from "@/common/components/buttons/BtMyPageCommunity";
+import { BtPopup } from "@/common/components/buttons/BtPopup";
 import { BtToolDetailFeedback } from "@/common/components/buttons/BtToolDetailFeedback";
 import { BtCommunityCmtSummit } from "../common/components/buttons/BtCommunityCmtSummit";
 import { BtCommunityPostBookmark } from "../common/components/buttons/BtCommunityPostBookmark";
@@ -28,6 +29,9 @@ export default function Home() {
 			<BtToolDetailBookmark state="default" />
 			<BtToolDetailBookmark state="hover" />
 			<BtToolDetailBookmark state="active" />
+			<BtPopup variant="primary">버튼</BtPopup>
+			<BtPopup variant="outline">버튼</BtPopup>
+			<BtPopup variant="danger">네, 탈퇴할게요</BtPopup>
 			{/* 필요에 따라 더 많은 버튼을 추가할 수 있습니다. */}
 		</h1>
 	);
