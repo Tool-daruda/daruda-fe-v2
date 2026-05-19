e8af69233049991cbbf95ff25321a0f1c9161510
imddoy <imddoy@duksung.ac.kr>
2026-05-19 17:06:04 +0900

    feat(client): 검색창 진입 연결

--- apps/client/src/app/toollist/_components/search-bar.tsx
+++ apps/client/src/app/toollist/_components/search-bar.tsx
@@ -5,12 +5,24 @@ export const SearchBar = () => {
 	return (
 		<header className={s.container}>
 			<h1 className={s.title}>daruda 툴 리스트</h1>
-			<div className={s.inputWrapper}>
-				<input type="text" placeholder="툴과 관련된 정보를 찾아드려요" className={s.input} />
-				<span className={s.searchIcon}>
+
+			<form action="/search" method="GET" className={s.inputWrapper}>
+				<input
+					type="text"
+					name="keyword"
+					placeholder="툴과 관련된 정보를 찾아드려요"
+					className={s.input}
+					required
+				/>
+
+				<button
+					type="submit"
+					className={s.searchIcon}
+					style={{ background: "none", border: "none", cursor: "pointer", padding: 0 }}
+				>
 					<Image src="/icons/ic_search_iris300_20.svg" alt="Search Icon" width={20} height={20} />
-				</span>
-			</div>
+				</button>
+			</form>
 		</header>
 	);
 };
