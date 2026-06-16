158f079e3c224627a90e2648c96b71f12661ffbe
imddoy <imddoy@duksung.ac.kr>
2026-06-16 20:52:44 +0900

    chore(client): 피드백 섹션 UI 수정

--- apps/client/src/app/toollist/[slug]/_components/styles/tool-feedback-section.css.ts
+++ apps/client/src/app/toollist/[slug]/_components/styles/tool-feedback-section.css.ts
@@ -8,11 +8,13 @@ export const container = style({
 export const title = style({
 	...themeVars.fonts.t4_1,
 	color: colors.grayscale[900],
+	marginBottom: "6px",
 });
 
 export const description = style({
 	...themeVars.fonts.caption2_1,
 	color: colors.grayscale[300],
+	marginBottom: "24px",
 });
 
 export const button = style({
@@ -24,4 +26,21 @@ export const button = style({
 	color: colors.brand.iris[500],
 	fontWeight: 700,
 	cursor: "pointer",
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+	justifyContent: "center",
+	gap: "6px",
+});
+
+export const buttonContent = style({
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
+	...themeVars.fonts.t4_1,
+	gap: "4px",
+});
+
+export const count = style({
+	...themeVars.fonts.b5_2,
 });
--- apps/client/src/app/toollist/[slug]/_components/tool-feedback-section.tsx
+++ apps/client/src/app/toollist/[slug]/_components/tool-feedback-section.tsx
@@ -1,14 +1,20 @@
+import Image from "next/image";
 import * as styles from "./styles/tool-feedback-section.css";
 
 export const ToolFeedbackSection = () => {
 	return (
 		<section className={styles.container}>
 			<h2 className={styles.title}>내용이 도움이 되었나요?</h2>
-			<p className={styles.description}>소개글이 서비스 이해에 도움이 되었는지 알려주세요.</p>
+			<p className={styles.description}>
+				효율적인 세상을 만들기 위해 다루다가 항상 노력하고 있어요.
+			</p>
 
 			<button type="button" className={styles.button}>
-				💜 도움이 되었어요
-				<p>1</p>
+				<div className={styles.buttonContent}>
+					<Image src="/icons/ic_heart_iris500_20.svg" width={20} height={20} alt="하트 아이콘" />
+					<span>도움이 되었어요</span>
+				</div>
+				<span className={styles.count}>1</span>
 			</button>
 		</section>
 	);
