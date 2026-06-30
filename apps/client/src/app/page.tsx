bc61bbd76638530d8a7126da6139f94b92d0522e
imddoy <imddoy@duksung.ac.kr>
2026-06-30 17:08:04 +0900

    chore(client): main 컴포넌트 그룹핑

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,8 +1,8 @@
-import { FreeToolsSection } from "./_components/free-tools-section";
-import { HeroSection } from "./_components/hero-section";
-import { PopularPostsSection } from "./_components/popular-posts-section";
-import { PopularToolsSection } from "./_components/popular-tools-section";
-import { RecentToolsSection } from "./_components/recent-tools-section";
+import { FreeToolsSection } from "./(main)/_components/free-tools-section";
+import { HeroSection } from "./(main)/_components/hero-section";
+import { PopularPostsSection } from "./(main)/_components/popular-posts-section";
+import { PopularToolsSection } from "./(main)/_components/popular-tools-section";
+import { RecentToolsSection } from "./(main)/_components/recent-tools-section";
 import * as s from "./page-style.css";
 
 export default function Home() {
